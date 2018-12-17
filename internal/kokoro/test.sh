85e4601271157fbc313cbdc6c5bf2646db7e5886
Jean de Klerk <deklerk@google.com>
2018-12-17 13:11:10 -0800

    internal: add kokoro resources (#122)

---
+++ internal/kokoro/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# TODO(deklerk) Add integration tests when it's secure to do so. b/64723143
+
+# Fail on any error
+set -eo pipefail
+
+# Display commands being run
+set -x
+
+# cd to project dir on Kokoro instance
+cd github/gax-go
+
+go version
+
+# Set $GOPATH
+export GOPATH="$HOME/go"
+export GENPROTO_HOME=$GOPATH/src/google/go-genproto
+export PATH="$GOPATH/bin:$PATH"
+mkdir -p $GENPROTO_HOME
+
+# Move code into $GOPATH and get dependencies
+git clone . $GENPROTO_HOME
+cd $GENPROTO_HOME
+
+try3() { eval "$*" || eval "$*" || eval "$*"; }
+try3 go get -v -t ./...
+
+./internal/kokoro/vet.sh
+
+# Run tests and tee output to log file, to be pushed to GCS as artifact.
+go test -race -v ./... 2>&1 | tee $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
