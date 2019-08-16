820a55d7189be0932d34739f8be3b4ba212812ea
Alex Hong <hongalex@google.com>
2019-08-15 17:36:01 -0700

    regen

--- googleapis/cloud/automl/v1beta1/image.pb.go
+++ googleapis/cloud/automl/v1beta1/image.pb.go
@@ -239,9 +239,16 @@ type ImageObjectDetectionModelMetadata struct {
 	// `train_cost` will be equal or less than this value. If further model
 	// training ceases to provide any improvements, it will stop without using
 	// full budget and the stop_reason will be `MODEL_CONVERGED`.
-	// Note, node_hour  = actual_hour * number_of_nodes_invovled. The train budget
-	// must be between 20,000 and 2,000,000 milli node hours, inclusive. The
-	// default value is 216, 000 which represents one day in wall time.
+	// Note, node_hour  = actual_hour * number_of_nodes_invovled.
+	// For model type `cloud-high-accuracy-1`(default) and `cloud-low-latency-1`,
+	// the train budget must be between 20,000 and 2,000,000 milli node hours,
+	// inclusive. The default value is 216, 000 which represents one day in
+	// wall time.
+	// For model type `mobile-low-latency-1`, `mobile-versatile-1`,
+	// `mobile-high-accuracy-1`, `mobile-core-ml-low-latency-1`,
+	// `mobile-core-ml-versatile-1`, `mobile-core-ml-high-accuracy-1`, the train
+	// budget must be between 1,000 and 100,000 milli node hours, inclusive.
+	// The default value is 24, 000 which represents one day in wall time.
 	TrainBudgetMilliNodeHours int64 `protobuf:"varint,6,opt,name=train_budget_milli_node_hours,json=trainBudgetMilliNodeHours,proto3" json:"train_budget_milli_node_hours,omitempty"`
 	// Output only. The actual train cost of creating this model, expressed in
 	// milli node hours, i.e. 1,000 value in this field means 1 node hour.
